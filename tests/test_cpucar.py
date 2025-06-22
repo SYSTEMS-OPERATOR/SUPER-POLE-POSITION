3910405990a5d86b6fe0d0103a5ea6a0196b4685
SYSTEMS-OPERATOR <155610697+SYSTEMS-OPERATOR@users.noreply.github.com>
2025-06-22 02:50:12 -0400

    Improve CPU car blocking

--- tests/test_cpucar.py
+++ tests/test_cpucar.py
@@ -1,10 +1,12 @@
 from super_pole_position.ai_cpu import CPUCar
 from super_pole_position.physics.car import Car
+from super_pole_position.physics.track import Track
 
 
 def test_cpucar_blocking():
+    track = Track(width=200.0, height=100.0)
     cpu = CPUCar(x=0.0, y=1.0)
     player = Car(x=-6.0, y=1.2, speed=5.0)
-    assert cpu.blocking(player)
+    assert cpu.blocking(player, track)
     player.y = 2.0
-    assert not cpu.blocking(player)
+    assert not cpu.blocking(player, track)
